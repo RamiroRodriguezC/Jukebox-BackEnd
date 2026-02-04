261185c66ee250878dcf3d73d8ce89e39b2d33d0
RamiroRodriguezC <ramirorodcas1112@gmail.com>
2026-02-03 23:30:15 -0300

    lista fix

--- src/routes/listaRoutes.js
+++ src/routes/listaRoutes.js
@@ -1,11 +1,12 @@
 const app = require('express');
 const listaController = require('../controllers/listaController');
+const { authenticateToken, isAdmin,isAuthor} = require('../middlewares/authMiddleware');
 const router = app.Router();
 
-router.post('/', listaController.crearNuevaLista);
-router.delete('/:listaId', listaController.eliminarLista);
-router.post('/:listaId/items', listaController.addItem);
-router.delete('/:listaId/items/:itemId', listaController.removeItem);
+router.post('/', listaController.crearNuevaLista,authenticateToken,isAuthor);
+router.delete('/:listaId', listaController.eliminarLista,authenticateToken,isAuthor,isAdmin);
+router.post('/:listaId/items', listaController.addItem,authenticateToken,isAuthor,isAdmin);
+router.delete('/:listaId/items/:itemId', listaController.removeItem,authenticateToken,isAuthor,isAdmin);
 
 
 module.exports = router;
